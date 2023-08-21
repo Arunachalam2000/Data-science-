15e472c0fabb83f7b93f999869f3be301598763c
Arunachalam2000 <142770420+Arunachalam2000@users.noreply.github.com>
2023-08-21 11:00:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const express = require('express');
-const app = express();
-const controllers = require('../controllers');
-const middlewares = require('../middlewares');
+const date = require("../date.js");
 
-// Mounting routes with middlewares and controllers
-app.get("/", controllers.getTodoList)
+var items = []; // Variables serving as local database instead of an actual database
+var workítems = [];
 
-app.post("/", middlewares.validateRequestBody, controllers.createTodoItem);
+async function getTodoList (req, res) {
+    const today = date.getDate();
+    const pageData = {
+        listTitle: today,
+        newListItems: items
+      };
 
-app.get("/work", controllers.getWorkList);
+  res.render("list", pageData); //render from the list of view folder
+}
 
-module.exports = app;
+async function createTodoItem (req, res) {
+    const item = req.body.newItem;
+    if (req.body.list && req.body.list === "Work") {
+        workítems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+}
+
+async function getWorkList (req, res) {
+    const pageData = {
+        listTitle: "Work List",
+        newListItems: workítems
+    };
+
+    res.render("list", pageData);
+}
+
+module.exports = {getTodoList, getWorkList, createTodoItem};
